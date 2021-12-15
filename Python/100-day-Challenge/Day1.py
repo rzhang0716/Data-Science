7ff0c9f3ce94c8fcb5b70c4d9aad495a3f6b6204
Ran <rzhang0716@gmail.com>
2021-12-15 16:46:53 -0500

    Add files via upload

---
+++ Python/100-day-Challenge/Day1.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print('Hello My Friend!')
+#2. Ask the user for the city that they grew up in.
+city = input('What is the city you grew up?\n')
+#3. Ask the user for the name of a pet.
+pet = input('What is the name your pet?\n')
+
+#4. Combine the name of their city and pet and show them their band name.
+print(city + ' ' + pet)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
