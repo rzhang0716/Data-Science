4073592a3d50afa8d0e95734d3f99ceb0e277cb0
Ran <rzhang0716@gmail.com>
2022-01-31 09:50:23 -0500

    Add files via upload

---
+++ Python/100-day-Challenge/Day30_Errors_Json/NATO_Phonetic_Alphabet_Improved/main.py
@@ -0,0 +1,24 @@
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+# Create a dictionary in this format
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+# Create a list of the phonetic code words from a word that the user inputs.
+
+word = input("Enter a word: ").upper()
+output_list = []
+for letter in word:
+    try:
+        new_word = phonetic_dict[letter]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        word = input("Enter a word: ").upper()
+    else:
+        output_list.append(new_word)
+
+print(output_list)
